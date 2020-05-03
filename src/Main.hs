837591fa949a001fcfa876bfb256fc3c992fa76b
Kamil Dworakowski <kamil@dworakowski.name>
2020-05-03 16:47:15 +0200

    use optparse-applicative

--- src/Main.hs
+++ src/Main.hs
@@ -3,30 +3,46 @@
 module Main where
 
 import qualified Data.ByteString.Char8 as B
+import qualified Data.Set as Set
 import qualified Data.UUID as UUID
+import Options.Applicative
 import Snap.Core
 import Snap.Http.Server
 import Snap.Util.FileServe
 import Snap.Util.GZip
 import System.Environment (getArgs)
 import System.Random
-import Data.List
-import qualified Data.Set as Set
 
 main :: IO ()
 main = do
-  [dirToServe] <- getArgs
+  (port, dirToServe) <- execParser cliParser
   uuid <- UUID.toASCIIBytes <$> randomIO
-  print config
-  putStrLn $ "serving on: " ++ "http://localhost:8787/" ++ B.unpack uuid ++ "/"
-  httpServe config
-      . withCompression' (Set.insert "text/csv" compressibleMimeTypes)
-      . dir uuid
-      $ serveDirectoryWith fancyDirectoryConfig dirToServe
-
+  let conf = config port
+  print conf
+  putStrLn $ "serving on: " ++ "http://localhost:" ++ show port ++ "/" ++ B.unpack uuid ++ "/"
+  httpServe conf
+    . withCompression' (Set.insert "text/csv" compressibleMimeTypes)
+    . dir uuid
+    $ serveDirectoryWith fancyDirectoryConfig dirToServe
   where
-    config =
-        setErrorLog ConfigNoLog
+    config port =
+      setErrorLog ConfigNoLog
         . setAccessLog ConfigNoLog
-        . setPort 8787
+        . setPort port
         $ defaultConfig
+
+cliParser =
+  info
+    (helper <*> options)
+    ( fullDesc
+        <> header "Ad-hoc HTTP file server" -- I have no idea where or when it displays
+        <> progDesc "Serve a directory under a randomly generated GUID"
+    )
+
+options :: Parser (Port, FilePath)
+options =
+  (,)
+    <$> option auto (long "port" <> short 'p' <> value 7878 <> showDefault <> metavar "INT")
+    <*> argument str (metavar "DIR" <> help "The directory to serve")
+
+type Port = Int
